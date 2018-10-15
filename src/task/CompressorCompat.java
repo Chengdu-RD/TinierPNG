74c83d07936dc47c59357a84b3cf96bbebfe3080
xiayong <xiayong@meituan.com>
2018-10-15 16:39:20 +0800

    完善输出格式

--- src/task/CompressorCompat.java
+++ src/task/CompressorCompat.java
@@ -1,13 +1,17 @@
 package task;
 
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import utils.CommonUtils;
 
-import java.util.Collection;
+import java.util.*;
 
 public class CompressorCompat {
     private Compressor mCompressor;
@@ -20,7 +24,7 @@ public class CompressorCompat {
         Task.Backgroundable task = new Task.Backgroundable(project, "Compress PNG Files", true) {
             @Override
             public void run(@NotNull ProgressIndicator progressIndicator) {
-
+                createOptimizeRunnable(progressIndicator, files, showBalloon, project).run();
             }
         };
 
@@ -33,7 +37,31 @@ public class CompressorCompat {
 
     private Runnable createOptimizeRunnable(ProgressIndicator progressIndicator, Collection<VirtualFile> files, boolean showBalloon, Project project) {
         return () -> {
+            long optimizedSize = 0L;
+            int optmizedFiles = 0;
+            LinkedList<VirtualFile> pngFiles = new LinkedList<>(files);
+            while (!pngFiles.isEmpty()) {
+                VirtualFile pngFile = pngFiles.pop();
+                if (progressIndicator != null) {
+                    progressIndicator.checkCanceled();
+                    progressIndicator.setText(pngFile.getPath());
+                }
 
+                if (pngFile.isDirectory()) {
+                    pngFile.getChildren();
+                    pngFiles.addAll(Arrays.asList(pngFile.getChildren()));
+                } else if (CommonUtils.isPngFile(pngFile)) {
+                    long profit = mCompressor.compress(project, pngFile);
+                    if (profit > 0) {
+                        optimizedSize += profit;
+                        optmizedFiles++;
+                    }
+                }
+            }
+            if (showBalloon) {
+                String message = optmizedFiles + " files were optimized<br/>" + CommonUtils.formatByte(optimizedSize, true) + " saved";
+                (new NotificationGroup("PNG Optimizer", NotificationDisplayType.BALLOON, true)).createNotification(message, NotificationType.INFORMATION).notify(project);
+            }
         };
     }
 }
